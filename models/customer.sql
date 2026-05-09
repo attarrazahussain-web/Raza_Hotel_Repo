088489255059bd5329e5ec7f6c67646fd634d670
ATTAR RAZA HUSSAIN <attarrazahussain@gmail.com>
2026-05-09 11:44:40 +0000

    Add Fact table

--- models/customer.sql
+++ models/customer.sql
@@ -3,14 +3,14 @@
     snowflake_warehouse='PC_DBT_WH'
 )}}
 with customer as (
-select C_CUSTKEY as Cust_Key, 
-C_NAME as Cust_Name,
-C_ADDRESS as Cust_addrress,
-C_NATIONKEY as Cust_NataionKey,
-C_PHONE as Cust_Phone,
-C_ACCTBAL as Cust_Account_Balance,
-C_MKTSEGMENT as Cust_Mktsegment,
-C_COMMENT  as Cust_Comment
+select C_CUSTKEY as cust_key, 
+C_NAME as cust_name,
+C_ADDRESS as cust_addrress,
+C_NATIONKEY as cust_nationkey
+C_PHONE as cust_Phone,
+C_ACCTBAL as cust_account_balance,
+C_MKTSEGMENT as cust_mktsegment,
+C_COMMENT  as cust_Comment
 from {{ source('snowflake_sample_data','customer')}}
 ) 
 
